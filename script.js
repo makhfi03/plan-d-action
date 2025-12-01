1f38883f485f66bffad8dfd4f40e161baef3508c
makhfi03 <ghita.makhfi.21@gmail.com>
2025-12-01 20:46:36 +0100

    plan d'action 2: partie 1

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 //exercice1
-console.log(localStorage);
-localStorage.setItem("name", "Ali");
-localStorage.getItem("name");
+// console.log(localStorage);
+// localStorage.setItem("name", "Ali");
+// localStorage.getItem("name");
 
 //exercice2
+// console.log(localStorage);
+// localStorage.setItem("name", "Ali");
+// localStorage.getItem("name");
+
+//Manipulation des tableaux
+//Partie 1 : Bases des tableaux
+// let tableau = ["ghita", "salma", "rim", "nisrin", "siham"];
+// console.log(tableau[0], tableau[4])
+// tableau.splice(1,1,"hanaa")
+// console.log(tableau)
+
