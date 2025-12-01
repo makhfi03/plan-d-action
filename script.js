f5ec5d2ae7bdeaaf3c43250a1a56923ec413479c
makhfi03 <ghita.makhfi.21@gmail.com>
2025-12-01 21:21:57 +0100

    partie 2: manipulation des tableaux

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-//exercice1
+//Plan d'action 1: LocalStorage
+//Partie 1 : Introduction + Pré-requis
 // console.log(localStorage);
 // localStorage.setItem("name", "Ali");
 // localStorage.getItem("name");
 
-//exercice2
+//Partie 2 :  Méthodes LocalStorage
 // console.log(localStorage);
 // localStorage.setItem("name", "Ali");
 // localStorage.getItem("name");
 
-//Manipulation des tableaux
+//Plan d'action 2: Manipulation des tableaux
 //Partie 1 : Bases des tableaux
 // let tableau = ["ghita", "salma", "rim", "nisrin", "siham"];
 // console.log(tableau[0], tableau[4])
 // tableau.splice(1,1,"hanaa")
 // console.log(tableau)
 
+//Partie 2 : Méthodes simples (push, pop, shift, unshift)
+// let tableau2 = ["a", "b", "c", "d"];
+// console.log(tableau2)
+// tableau2.push("aa")
+// console.log(tableau2)
+// tableau2.unshift("zz")
+// console.log(tableau2)
+// tableau2.shift()
+// console.log(tableau2)
+// tableau2.pop()
+// console.log(tableau2)
+
+//LIFO (Last In, First Out)
+// let pile = [];
+// pile.push("hi")
+// pile.push("world")
+// pile.push("!")
+// console.log(pile)
+
+// pile.pop()
+// console.log(pile)
+// pile.pop()
+// console.log(pile)
+// pile.pop()
+// console.log(pile)
+
+//FIFO (First In, First Out)
+// let file = [];
+// pile.push("hi")
+// pile.push("world")
+// pile.push("!")
+// console.log(pile)
+
+// pile.shift()
+// console.log(pile)
+// pile.shift()
+// console.log(pile)
+// pile.shift()
+// console.log(pile)
